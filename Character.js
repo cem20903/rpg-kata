f636d1e761b07f5c4576b4a23ec124c85dbd34d0
David Casas Arenas <dcasasar@HUAWEI_P_smart-13200a85ab.Home>
2022-04-04 09:51:38 +0100

    Add attack and heal

--- Character.js
+++ Character.js
@@ -2,7 +2,25 @@ class Character {
   constructor() {
     this.level = 1;
     this.health = 1000;
-    this.status = "alive";
+    this.alive = true;
+  }
+
+  attack(target) {
+    if (target.health - 100 <= 0) {
+      target.health = 0;
+      target.alive = false;
+      return;
+    }
+    target.health = target.health - 100;
+  }
+
+  heal() {
+    if (this.health + 100 >= 1000) {
+      this.health = 1000;
+      return;
+    }
+
+    this.health = this.health + 100;
   }
 }
 
--- character.spec.js
+++ character.spec.js
@@ -5,7 +5,7 @@ it("The character start with 1000 live, level 1 and status live", () => {
 
   expect(character.health).toEqual(1000);
   expect(character.level).toEqual(1);
-  expect(character.status).toEqual("alive");
+  expect(character.alive).toEqual(true);
 });
 
 it("A character can attack another character with 100 damage", () => {
@@ -13,6 +13,37 @@ it("A character can attack another character with 100 damage", () => {
   const characterTwo = new Character();
 
   characterOne.attack(characterTwo);
+
+  expect(characterTwo.health).toEqual(900);
+});
+
+it("A character health will be 0 when the damage exceeds its health", () => {
+  const characterOne = new Character();
+  const characterTwo = new Character();
+
+  for (let i = 10; i >= 0; i--) {
+    characterOne.attack(characterTwo);
+  }
+
+  expect(characterTwo.health).toEqual(0);
+  expect(characterTwo.alive).toEqual(false);
+});
+
+it("A character cannot heal if it has 1000 of health", () => {
+  const characterOne = new Character();
+
+  characterOne.heal();
+
+  expect(characterOne.health).toEqual(1000);
+});
+
+it("A character can heal if it has less than 1000 of health", () => {
+  const characterOne = new Character();
+  const characterTwo = new Character();
+
+  characterOne.attack(characterTwo);
+  characterOne.attack(characterTwo);
+  characterTwo.heal();
 
   expect(characterTwo.health).toEqual(900);
 });
