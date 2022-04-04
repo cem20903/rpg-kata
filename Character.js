971c3b4f138f61b33998f63ee6e469b0504fdc66
David Casas Arenas <dcasasar@MacBook-Pro-de-David.local>
2022-04-04 11:10:46 +0100

    Fran perea el que lo lea

--- Character.js
+++ Character.js
@@ -1,25 +1,32 @@
 const ATTACK_POINTS = 100;
+const MIN_ATTACK_POINTS = ATTACK_POINTS / 2;
+const MAX_ATTACK_POINTS = ATTACK_POINTS + ATTACK_POINTS * 0.5;
 const HEAL_POINTS = 100;
+const INITAL_HEALTH = 1000;
 const ALIVE = true;
 const DEAD = false;
+
 class Character {
-  constructor() {
+  constructor(id) {
+    this.id = id;
     this.level = 1;
-    this.health = 1000;
+    this.health = INITAL_HEALTH;
     this.alive = ALIVE;
-  }
-
-  _isCharacterDead(target) {
-    return target.health - ATTACK_POINTS <= 0;
-  }
-
-  _canCharacterBeHeal() {
-    return this.health + HEAL_POINTS < 1000;
+    this.factions = [];
   }
 
   attack(target) {
+    if (target.id === this.id) return;
     if (this._isCharacterDead(target)) {
       this._killCharacter(target);
+      return;
+    }
+    if (this._useMinAttack(target)) {
+      target.health -= MIN_ATTACK_POINTS;
+      return;
+    }
+    if (this._useMaxAttack(target)) {
+      target.health -= MAX_ATTACK_POINTS;
       return;
     }
     target.health -= ATTACK_POINTS;
@@ -28,16 +35,36 @@ class Character {
   heal() {
     if (!this.alive) return;
     if (!this._canCharacterBeHeal()) {
-      this.health = 1000;
+      this.health = INITAL_HEALTH;
       return;
     }
 
     this.health += HEAL_POINTS;
   }
 
+  joinsFactions(faction) {
+    this.factions.push(faction);
+  }
+
   _killCharacter(target) {
     target.health = 0;
     target.alive = DEAD;
+  }
+
+  _isCharacterDead(target) {
+    return target.health - ATTACK_POINTS <= 0;
+  }
+
+  _canCharacterBeHeal() {
+    return this.health + HEAL_POINTS < INITAL_HEALTH;
+  }
+
+  _useMaxAttack(target) {
+    return this.level - target.level >= 5;
+  }
+
+  _useMinAttack(target) {
+    return target.level - this.level >= 5;
   }
 }
 
--- character.spec.js
+++ character.spec.js
@@ -1,7 +1,7 @@
 const Character = require("./Character.js");
 
 it("The character start with 1000 live, level 1 and status live", () => {
-  const character = new Character();
+  const character = new Character(1);
 
   expect(character.health).toEqual(1000);
   expect(character.level).toEqual(1);
@@ -9,8 +9,8 @@ it("The character start with 1000 live, level 1 and status live", () => {
 });
 
 it("A character can attack another character with 100 damage", () => {
-  const characterOne = new Character();
-  const characterTwo = new Character();
+  const characterOne = new Character(1);
+  const characterTwo = new Character(2);
 
   characterOne.attack(characterTwo);
 
@@ -18,8 +18,8 @@ it("A character can attack another character with 100 damage", () => {
 });
 
 it("A character health will be 0 when the damage exceeds its health", () => {
-  const characterOne = new Character();
-  const characterTwo = new Character();
+  const characterOne = new Character(1);
+  const characterTwo = new Character(2);
 
   for (let i = 10; i >= 0; i--) {
     characterOne.attack(characterTwo);
@@ -30,33 +30,81 @@ it("A character health will be 0 when the damage exceeds its health", () => {
 });
 
 it("A character cannot heal if it has 1000 of health", () => {
-  const characterOne = new Character();
+  const characterOne = new Character(1);
 
   characterOne.heal();
 
   expect(characterOne.health).toEqual(1000);
 });
 
-it("A character cannot heal if it's dead", () => {
-  const characterOne = new Character();
-  const characterTwo = new Character();
+it("A character can heal if it has less than 1000 of health", () => {
+  const characterOne = new Character(1);
 
-  for (let i = 10; i >= 0; i--) {
-    characterOne.attack(characterTwo);
-  }
+  characterOne.health = 800;
+  characterOne.heal();
 
-  characterTwo.heal();
-
-  expect(characterTwo.health).toEqual(0);
+  expect(characterOne.health).toEqual(900);
 });
 
-it("A character can heal if it has less than 1000 of health", () => {
-  const characterOne = new Character();
-  const characterTwo = new Character();
+it("A character cannot deal damage to itslef", () => {
+  const characterOne = new Character(1);
+
+  characterOne.attack(characterOne);
+
+  expect(characterOne.health).toEqual(1000);
+});
+
+it("A character cannot heal another character", () => {
+  const characterOne = new Character(1);
+  const characterTwo = new Character(2);
 
   characterOne.attack(characterTwo);
-  characterOne.attack(characterTwo);
-  characterTwo.heal();
+  characterOne.heal(characterTwo);
 
   expect(characterTwo.health).toEqual(900);
+});
+
+it("A character damage is reduced by 50% if the difference between levels is 5 or more ", () => {
+  const characterOne = new Character(1);
+  const characterTwo = new Character(2);
+
+  characterTwo.level = 6;
+
+  characterOne.attack(characterTwo);
+
+  expect(characterTwo.health).toEqual(950);
+});
+
+it("A character damage is increased by 50% if the difference between levels is 5 or more ", () => {
+  const characterOne = new Character(1);
+  const characterTwo = new Character(2);
+
+  characterOne.level = 6;
+
+  characterOne.attack(characterTwo);
+
+  expect(characterTwo.health).toEqual(850);
+});
+
+it("A character does not belong to an initial faction", () => {
+  const characterOne = new Character(1);
+
+  expect(characterOne.factions).toEqual([]);
+});
+
+it("A character can join a faction", () => {
+  const characterOne = new Character(1);
+
+  characterOne.joinsFactions("faction one");
+
+  expect(characterOne.factions).toEqual(["faction one"]);
+});
+
+it("A character can join two factions", () => {
+  const characterOne = new Character(1);
+
+  characterOne.joinsFactions("faction one");
+  characterOne.joinsFactions("faction two");
+
+  expect(characterOne.factions).toEqual(["faction one", "faction two"]);
 });
